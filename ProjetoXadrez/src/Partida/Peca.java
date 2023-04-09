7e904bc19b541d4620554ab877bce79757558c5a
LPreche <paulorecheme@hotmail.com>
2023-04-08 23:16:46 -0300

    Criada Classes das peças, tabuleiro e posições

---
+++ ProjetoXadrez/src/Partida/Peca.java
@@ -0,0 +1,17 @@
+package Partida;
+
+public class Peca {
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+    
+    public Peca(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    
+    
+}
---
+++ ProjetoXadrez/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import javax.swing.text.TabSet;
+
+import Partida.Posicao;
+import Partida.Tabuleiro;
+
+public class Program {
+    public static void main(String[] args){
+       Tabuleiro tabuleiro = new Tabuleiro(8,8 );
+        
+    }
+}
